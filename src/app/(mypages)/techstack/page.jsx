02ebe4dbc6fd394e1945ca096406aa637d044a9f
aryandewan <aryan.dewan.05@gmail.com>
2025-05-08 15:01:32 +0530

    new updates

--- src/app/(mypages)/techstack/page.jsx
+++ src/app/(mypages)/techstack/page.jsx
@@ -1,38 +1,44 @@
-import React from 'react'
 import Techstack from "@/app/(mypages)/techstack/techstack";
 
 const techStack = [
     {
+        icon: "react",
         name: "ReactJS",
         level: "Intermediate",
         levelClass: "h-full w-1/2 bg-[#ad0013] rounded-full"
     },
     {
+        icon: "next",
         name: "NextJS",
         level: "Beginner → Intermediate",
         levelClass: "h-full w-2/5 bg-[#ad0013] rounded-full"
     },
     {
+        icon: "tailwindCSS",
         name: "Tailwind CSS",
         level: "Intermediate",
         levelClass: "h-full w-1/2 bg-[#ad0013] rounded-full"
     },
     {
+        icon: "HTML",
         name: "HTML",
         level: "Intermediate → Expert",
         levelClass: "h-full w-4/5 bg-[#ad0013] rounded-full"
     },
     {
+        icon: "CSS",
         name: "CSS",
         level: "Intermediate → Expert",
         levelClass: "h-full w-4/5 bg-[#ad0013] rounded-full"
     },
     {
+        icon: "JS",
         name: "JavaScript",
         level: "Intermediate → Expert",
         levelClass: "h-full w-4/5 bg-[#ad0013] rounded-full"
     },
     {
+        icon: "TS",
         name: "TypeScript",
         level: "Beginner → Intermediate",
         levelClass: "h-full w-2/7 bg-[#ad0013] rounded-full"
--- src/app/(mypages)/techstack/techstack.jsx
+++ src/app/(mypages)/techstack/techstack.jsx
@@ -5,8 +5,22 @@ import Link from "next/link";
 import {FiArrowUpLeft} from "react-icons/fi";
 import SplitType from "split-type";
 import gsap from "gsap";
+import { FaReact, FaHtml5, FaCss3 } from "react-icons/fa";
+import { RiNextjsFill, RiTailwindCssFill} from "react-icons/ri";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiTypescript } from "react-icons/si";
 
 const Techstack = ({techStack}) => {
+    const iconMap = {
+        react: FaReact,
+        next: RiNextjsFill,
+        tailwindCSS: RiTailwindCssFill,
+        HTML: FaHtml5,
+        CSS: FaCss3,
+        JS: IoLogoJavascript,
+        TS: SiTypescript
+    };
+
     useLayoutEffect(() => {
         const splitTech = new SplitType('.tech', {
             types: 'lines, words, chars',
@@ -45,16 +59,23 @@ const Techstack = ({techStack}) => {
             </div>
             <div className="w-full flex items-center justify-center">
                 <div className="w-[85%] font-rubik flex flex-col items-center justify-center gap-10">
-                    {techStack.map((tech, index) => (
-                        <div className="w-full" key={index}>
-                            <div className="flex justify-between text-lg md:text-2xl lg:text-3xl text-[#a67d43] mb-1">
-                                <span className="tech">{tech.name}</span><span className="tech italic">{tech.level}</span>
+                    {techStack.map((tech, index) => {
+                        const IconComponent = iconMap[tech.icon];
+                        return (
+                            <div key={index} className="w-full">
+                                <div className="flex justify-between text-lg md:text-2xl lg:text-3xl text-[#a67d43] mb-1">
+                                    <span className="tech flex items-center justify-center">
+                                        <IconComponent className="mr-2 text-2xl" />
+                                        {tech.name}
+                                    </span>
+                                    <span className="tech italic">{tech.level}</span>
+                                </div>
+                                <div className="h-1.5 rounded-full overflow-hidden">
+                                    <div className={`${tech.levelClass} level`}></div>
+                                </div>
                             </div>
-                            <div className="h-1.5 rounded-full overflow-hidden">
-                                <div className={`${tech.levelClass} level`}></div>
-                            </div>
-                        </div>
-                    ))}
+                        );
+                    })}
                 </div>
             </div>
         </section>
