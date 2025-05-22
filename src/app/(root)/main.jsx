9e3ff3409156b5d27eb892725b5b289b1569bac5
n0te <96051651+aryandewan@users.noreply.github.com>
2025-05-22 20:05:44 +0530

    Update main.jsx

--- src/app/(root)/main.jsx
+++ src/app/(root)/main.jsx
@@ -10,7 +10,7 @@ import EntryAnim from "@/components/EntryAnim";
 
 const Main = () => (
     <EntryAnim>
-        <div id="container" className="grid grid-cols-1 xl:grid-cols-4 xl:grid-rows-4 w-full h-dvh gap-3 bg-[#1a1a1a] overflow-hidden">
+        <div id="container" className="grid grid-cols-1 xl:grid-cols-4 xl:grid-rows-4 w-full min-h-dvh xl:h-dvh gap-3 bg-[#1a1a1a] overflow-hidden">
             <Hero containerClass="bg-[#ad0013] xl:row-start-1 xl:row-span-4 xl:col-span-2 rounded-[70px] hero opacity-0 -translate-x-[100%]" sectionName="Hero" />
             <AboutMe containerClass="bg-[#a67d43] rounded-t-full rounded-bl-full me opacity-0 -translate-y-[100%]" sectionName="About Me" />
             <Projects containerClass="bg-[#121312] border-2 border-[#ad0013] !text-[#a67d43] xl:row-span-1 rounded-br-full rounded-t-full proj opacity-0 -translate-y-[100%]" sectionName="Projects" />
